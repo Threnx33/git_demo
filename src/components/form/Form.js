f8c040a0d7096361a223fb4715cd20459c1402b3
Turturica Dorin <turturica.dorin30@gmail.com>
2022-07-25 17:12:20 +0300

    Added input

---
+++ src/components/form/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <form>
+        <label>Nume: </label>
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
